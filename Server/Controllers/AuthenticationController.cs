c188fb0dc1ed523bb706a86da0e1abe28b4cd8a5
NguyenTranBaoKhanh <khanh1010vn@gmail.com>
2024-12-18 17:04:10 +0700

    Add register API and sign in API

---
+++ Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using BaseLibrary.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController(IUserAccount userAccountInterface) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> CreateAsync(Register user)
+        {
+            if(user == null) return BadRequest("Model is empty");
+            var result = await userAccountInterface.CreateAsync(user);
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> SignInAsync(Login user)
+        {
+            if (user is null) return BadRequest("Model is empty");
+            var result = await userAccountInterface.SignInAsync(user);
+            return Ok(result);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefeshTokenAsync(RefreshToken token)
+        {
+            if (token is null) return BadRequest("Model is empty");
+            var result = await userAccountInterface.RefreshTokenAsync(token);
+            return Ok(result);
+        }
+    }
+}
