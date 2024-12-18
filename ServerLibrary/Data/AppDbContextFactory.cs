6561ab9e156ab8866484ae6dc043d696dafcb14f
NguyenTranBaoKhanh <khanh1010vn@gmail.com>
2024-12-18 13:43:02 +0700

    Added entities and created database

---
+++ ServerLibrary/Data/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ServerLibrary.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")); // Replace with your actual connection string.
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
